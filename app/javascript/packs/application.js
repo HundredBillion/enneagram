5f9fdb3b45acb4c3af9c8ba7846ac24d3b01a2a1
DAVID LEE <davidlee@davids-mbp.lan>
2020-06-16 09:28:57 -0400

    update config file with AWS expert

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-require("enneagram_test")
+require("./enneagram_test")
 
 import "bootstrap"
 import "../stylesheets/application"
