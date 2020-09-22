2e2a61aeaf2dab13a43e4206a1289709c049d0eb
David <david@davids-mac-pro.lan>
2020-09-22 11:47:51 -0400

    new navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,14 +12,11 @@ require("packs/enneagram_test");
 // require("checkouts.js");
 
 import "bootstrap";
-import "stylesheets/application";
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap/dist/css/bootstrap";
+import "stylesheets/application";
 
-document.addEventListener("turbolinks:load", () => {
-  $('[data-toggle="tooltip"]').tooltip();
-  $('[data-toggle="popover"]').popover();
-});
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
